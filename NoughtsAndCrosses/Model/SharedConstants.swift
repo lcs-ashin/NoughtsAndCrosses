cd12423140b1df9e024d469a1686613e1d02d001
lcs-ashin <ashin@lcs.on.ca>
2022-05-02 15:13:35 -0400

    Set up a view to represent a single tile.

---
+++ NoughtsAndCrosses/Model/SharedConstants.swift
@@ -0,0 +1,12 @@
+//
+//  SharedConstants.swift
+//  NoughtsAndCrosses
+//
+//  Created by Eunbi Shin on 2022-05-02.
+//
+
+import Foundation
+//Possible states for the tile
+let nought = "o"
+let cross = "x"
+let empty = ""
