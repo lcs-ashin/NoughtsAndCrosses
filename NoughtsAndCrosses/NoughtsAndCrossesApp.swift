3f13ad65bd97f539816dc4f7b34dbc4b79a8cdba
lcs-ashin <ashin@lcs.on.ca>
2022-05-02 14:59:15 -0400

    Initial Commit

---
+++ NoughtsAndCrosses/NoughtsAndCrossesApp.swift
@@ -0,0 +1,17 @@
+//
+//  NoughtsAndCrossesApp.swift
+//  NoughtsAndCrosses
+//
+//  Created by Eunbi Shin on 2022-05-02.
+//
+
+import SwiftUI
+
+@main
+struct NoughtsAndCrossesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
