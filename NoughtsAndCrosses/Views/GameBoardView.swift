58a21fff8956ec46fd4db2f98031a77be50ba7a8
lcs-ashin <ashin@lcs.on.ca>
2022-05-02 15:32:11 -0400

    Game board has been laid out with a static interface. And added properties to keep track of the tile state on the game board. The source of truth uses @State (on game board) and each tile is connected using the @Binding property wrapper.

--- NoughtsAndCrosses/Views/GameBoardView.swift
+++ NoughtsAndCrosses/Views/GameBoardView.swift
@@ -8,8 +8,52 @@
 import SwiftUI
 
 struct GameBoardView: View {
+    
+    // MARK: Stored Properties
+    
+    // Game board state (all nine positions)
+    //top row
+    @State var upperLeft = empty
+    @State var upperMiddle = empty
+    @State var upperRight = empty
+    
+    //middle row
+    @State var middleLeft = empty
+    @State var middleMiddle = empty
+    @State var middleRight = empty
+    
+    //bottom row
+    @State var bottomLeft = empty
+    @State var bottomMiddle = empty
+    @State var bottomRight = empty
+    
+    
+    // MARK: Computed Properties
     var body: some View {
-        Text("This is the game board.")
+        VStack {
+            
+            // Top Row
+            HStack {
+                TileView(state: $upperLeft)
+                TileView(state: $upperMiddle)
+                TileView(state: $upperRight)
+            }
+            
+            // Middle Row
+            HStack {
+                TileView(state: $middleLeft)
+                TileView(state: $middleMiddle)
+                TileView(state: $middleRight)
+            }
+            
+            // Bottom Row
+            HStack {
+                TileView(state: $bottomLeft)
+                TileView(state: $bottomMiddle)
+                TileView(state: $bottomRight)
+            }
+            
+        }
     }
 }
 
